1ff05eedc5c3b8a623d47e1f1f9074dcd914bf50
FilipJJJ <Filip.jarlesjo@gmail.com>
2023-11-12 11:15:46 +0100

    Pop Up text trigger

---
+++ Assets/PopUpTextTrigger.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class PopUpTextTrigger : MonoBehaviour
+{
+    [SerializeField]
+    private string _message = "";
+
+    [SerializeField]
+    private TextMeshProUGUI _uiText;
+
+    [SerializeField]
+    private float _timeShown = 5f;
+    private float _timeSinceShown = 0;
+    private bool _beingShown = false;
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            _beingShown = true;
+            _uiText.text = _message;
+        }
+    }
+
+    private void Update()
+    {
+        if (_beingShown)
+        {
+            if(_timeShown < _timeSinceShown)
+            {
+                _timeSinceShown += Time.deltaTime;
+            }
+            else
+            {
+                _uiText.text = "";
+                _beingShown=false;
+            }
+        }
+    }
+}
